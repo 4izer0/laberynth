3baca5e7d26572e99c1094665aa58db7bf85210f
4izer0 <ismael.dante89@gmail.com>
2022-03-09 10:07:54 -0600

    09-03(persegidor)

---
+++ Assets/scrio/NPCagent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCagent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
